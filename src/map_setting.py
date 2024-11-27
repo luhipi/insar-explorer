b1f9c43625573eaaf45bee4386f1f597f9450257
mahmud1 <mahmud1@users.noreply.github.com>
2024-11-27 23:53:39 +0100

    split symbology. add symbology for raster.

--- src/map_setting.py
+++ src/map_setting.py
@@ -1,6 +1,8 @@
+import numpy as np
 from qgis.PyQt.QtGui import QColor
 from qgis.core import QgsGraduatedSymbolRenderer, QgsRendererRange, QgsSymbol
-import numpy as np
+from qgis.core import QgsRasterShader, QgsColorRampShader, QgsSingleBandPseudoColorRenderer
+
 
 from . import color_maps
 from . import layer_utils
@@ -27,15 +29,25 @@ class InsarMap:
         self.num_classes = 9
         self.color_ramp_name = 'Roma'
         self.color_ramp_reverse_flag = False
+        self.data_type = "vector"
 
     def setSymbologyRangeFromData(self, layer=None, n_std=None):
         if not layer:
             layer = self.iface.activeLayer()
 
-        status, message = layer_utils.checkVectorLayer(layer)
-        if status is False:
+        status_vector, message = layer_utils.checkVectorLayer(layer)
+        status_raster, message = layer_utils.checkGmtsarLayer(layer)
+        if status_vector:
+            self.data_type = "vector"
+            self.getDataRangeFromVectorLayer(layer, n_std)
+        elif status_raster:
+            self.data_type = "raster"
+            self.getDataRangeFromRasterLayer(layer, n_std)
+        else:
+            message = '<span style="color:red;">Invalid Layer: Please select a valid layer.</span>'
             return message
 
+    def getDataRangeFromVectorLayer(self, layer, n_std=None):
         field_name, message = layer_utils.checkVectorLayerVelocity(layer)
         if field_name is None:
             return message
@@ -61,6 +73,22 @@ class InsarMap:
 
         return ""
 
+    def getDataRangeFromRasterLayer(self, layer, n_std=None):
+        if n_std is None:
+            if self.data_min is None or self.data_max is None:
+                self.data_min = layer.dataProvider().bandStatistics(1).minimumValue
+                self.data_max = layer.dataProvider().bandStatistics(1).maximumValue
+            self.min_value = self.data_min
+            self.max_value = self.data_max
+        else:
+            if self.data_mean is None or self.data_stdv is None:
+                self.data_mean = layer.dataProvider().bandStatistics(1).mean
+                self.data_stdv = layer.dataProvider().bandStatistics(1).stdDev
+            self.min_value = self.data_mean - n_std * self.data_stdv
+            self.max_value = self.data_mean + n_std * self.data_stdv
+
+        return ""
+
     def setSymbology(self, layer=None, color_ramp_name=None):
 
         if not color_ramp_name:
@@ -69,26 +97,62 @@ class InsarMap:
         if not layer:
             layer = self.iface.activeLayer()
 
-        status, message = layer_utils.checkVectorLayer(layer)
-        if status is False:
+        status_vector, message = layer_utils.checkVectorLayer(layer)
+        status_raster, message = layer_utils.checkGmtsarLayer(layer)
+        if (status_vector and status_raster) is False:
+            message = '<span style="color:red;">Could not set the symbology. Check layer validity.</span>'
             return message
 
-        interval = (self.max_value - self.min_value) / self.num_classes
+        if status_vector or status_raster:
+            interval = (self.max_value - self.min_value) / self.num_classes
 
-        if color_ramp_name == 'Turbo':
-            color_ramp = color_maps.Turbo()
-        if color_ramp_name == 'Roma':
-            color_ramp = color_maps.Roma()
-        if color_ramp_name == 'Vik':
-            color_ramp = color_maps.Vik()
+            if color_ramp_name == 'Turbo':
+                color_ramp = color_maps.Turbo()
+            if color_ramp_name == 'Roma':
+                color_ramp = color_maps.Roma()
+            if color_ramp_name == 'Vik':
+                color_ramp = color_maps.Vik()
 
-        if self.color_ramp_reverse_flag:
-            color_ramp.reverse()
+            if self.color_ramp_reverse_flag:
+                color_ramp.reverse()
+
+            max_length = max(len(f"{self.min_value:.2f}"), len(f"{self.max_value:.2f}"))
+
+        if status_vector:
+            self.setSymbologyVector(layer, interval, max_length, color_ramp)
+            return ""
+        elif status_raster:
+            self.setSymbologyRaster(layer, interval, max_length, color_ramp)
+            return ""
+        else:
+            message = '<span style="color:red;">Could not set the symbology. Check layer validity.</span>'
+            return message
+
+    def setSymbologyRaster(self, layer, interval, max_length, color_ramp):
+
+        shader = QgsRasterShader()
+        color_ramp_shader = QgsColorRampShader()
+        color_ramp_shader.setColorRampType(QgsColorRampShader.Interpolated)
+
+        color_ramp_items = []
+        for i in range(self.num_classes):
+            value = self.min_value + i * interval
+            color_ratio = float(i) / (self.num_classes - 1)
+            color = color_ramp.getColor(color_ratio)
+            color.setAlphaF(self.alpha)
+            color_ramp_items.append(QgsColorRampShader.ColorRampItem(value, color))
+
+        color_ramp_shader.setColorRampItemList(color_ramp_items)
+        shader.setRasterShaderFunction(color_ramp_shader)
+
+        renderer = QgsSingleBandPseudoColorRenderer(layer.dataProvider(), 1, shader)
+        layer.setRenderer(renderer)
+        layer.triggerRepaint()
+        self.iface.mapCanvas().refresh()
+
+    def setSymbologyVector(self, layer, interval, max_length, color_ramp):
 
         ranges = []
-
-        max_length = max(len(f"{self.min_value:.2f}"), len(f"{self.max_value:.2f}"))
-
         for i in range(self.num_classes):
             lower = self.min_value + i * interval
             upper = lower + interval
@@ -112,27 +176,6 @@ class InsarMap:
             range_item = QgsRendererRange(lower, upper, symbol, label)
             ranges.append(range_item)
 
-        # lower_symbol = QgsSymbol.defaultSymbol(layer.geometryType())
-        # color = color_ramp.getColor(0)
-        # color.setAlphaF(self.alpha)
-        # lower_symbol.setColor(color)
-        # lower_symbol.setSize(self.symbol_size)
-        # lower_symbol.symbolLayer(0).setStrokeWidth(self.stroke_width)
-        # lower_symbol.symbolLayer(0).setStrokeColor(QColor("gray"))
-        # lower_range = QgsRendererRange(float('-inf'), self.min_value, lower_symbol, f"< {self.min_value:.2f}")
-        # ranges.insert(0, lower_range)
-        #
-        # upper_symbol = QgsSymbol.defaultSymbol(layer.geometryType())
-        # color = color_ramp.getColor(1)
-        # color.setAlphaF(self.alpha)
-        # upper_symbol.setColor(color)
-        # upper_symbol.setSize(self.symbol_size)
-        # upper_symbol.symbolLayer(0).setStrokeWidth(self.stroke_width)
-        # upper_symbol.symbolLayer(0).setStrokeColor(QColor("gray"))
-        # upper_range = QgsRendererRange(self.max_value, float('inf'), upper_symbol, f"> {self.max_value:.2f}")
-        # ranges.append(upper_range)
-
-        # TODO: add support for different processors
         field_name, message = layer_utils.checkVectorLayerVelocity(layer)
         if field_name is None:
             return message
