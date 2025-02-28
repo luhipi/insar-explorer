068c04cc76287b7381c0d308305f06794cc30261
mahmud1 <mahmud1@users.noreply.github.com>
2025-03-01 00:39:33 +0100

    show fields with int type in field selector

--- src/gui_controller.py
+++ src/gui_controller.py
@@ -51,12 +51,6 @@ class GuiController(QObject):
 
         field_list, field_types = vector_layer_utils.getVectorFields(layer)
         velocity_field, message = vector_layer_utils.getVectorVelocityFieldName(layer)
-        # self.ui.cb_select_field.clear()
-        # field_list_to_display = [
-        #     field for field, type_name in zip(field_list, field_types)
-        #     if type_name == 'Real'
-        # ]
-        # self.ui.cb_select_field.addItems(field_list_to_display)
 
         for field, type_name in zip(field_list, field_types):
             self.ui.cb_select_field.addItem(field)
