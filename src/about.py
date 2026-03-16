6dce2e193ed7cbc8e6a4b9174e9191e99c1a1b12
mahmud1 <mahmud1@users.noreply.github.com>
2026-03-16 11:53:42 +0100

    fix lint

--- src/about.py
+++ src/about.py
@@ -30,8 +30,8 @@ about = f"""
                             in
                             <em>Proc.&nbsp;2025 IEEE Int.&nbsp;Geosci.&nbsp;Remote Sens.&nbsp;Symp.&nbsp;(IGARSS)</em>,
                             Brisbane,&nbsp;Australia, 2025, pp.&nbsp;9414–9417.&nbsp;
-                            <a href="https://ieeexplore.ieee.org/abstract/document/11313961"> 
-                                doi: 10.1109/IGARSS55030.2025.11313961 
+                            <a href="https://ieeexplore.ieee.org/abstract/document/11313961">
+                                doi: 10.1109/IGARSS55030.2025.11313961
                             </a>
                         </cite>
                     </li>
