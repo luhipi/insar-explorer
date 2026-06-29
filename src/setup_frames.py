def2a9ba20e172f0ce19d20da73a275f447bc1bd
mahmud1 <mahmud1@users.noreply.github.com>
2026-06-29 11:10:29 +0200

    drop Matplotlib compatibility aliases from plot frame setup
    migrate to pyqtgraph: setup pyqtgraph frame

--- src/setup_frames.py
+++ src/setup_frames.py
@@ -1,31 +1,16 @@
-from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
-from matplotlib.figure import Figure
-import matplotlib.pyplot as plt
+from ..external import pyqtgraph as pg
 
 from qgis.PyQt.QtWidgets import QVBoxLayout
 
-from .mpl_toolbar import CustomToolbar
+from .pg_toolbar import CustomToolbar
 
 
 def setupTsFrame(ui):
-    print(ui)
-    plt.style.use('bmh')
-    ui.figure = Figure()
-    ui.canvas = FigureCanvas(ui.figure)
-    ui.toolbar = CustomToolbar(ui.canvas, ui.frame_plot_ts)
+    ui.plot_widget = pg.GraphicsLayoutWidget(parent=ui.frame_plot_ts)
+    ui.plot_widget.setBackground('w')
+    ui.plot_widget.plot_items = []
+    ui.toolbar = CustomToolbar(ui.plot_widget, ui.frame_plot_ts)
 
     ui.frame_plot_layout = QVBoxLayout(ui.frame_plot_ts)
-    ui.frame_plot_layout.addWidget(ui.canvas)
+    ui.frame_plot_layout.addWidget(ui.plot_widget)
     ui.frame_plot_layout.addWidget(ui.toolbar)
-
-
-# from PyQt5.QtWebKitWidgets import QWebView
-# from PyQt5.QtWidgets import QVBoxLayout
-#
-# def setupTsFrame(ui):
-#     """
-#     Setup the time series frame to contain the plot
-#     """
-#     ui.web_view = QWebView()
-#     ui.frame_plot_layout = QVBoxLayout(ui.frame_plot_ts)
-#     ui.frame_plot_layout.addWidget(ui.web_view)
