b0b2db400791697682d9d6a1685b297e93dc4960
mahmud1 <mahmud1@users.noreply.github.com>
2026-07-10 23:07:09 +0200

    time series toolbar: integrate TimeSeriesToolbar into time series panel

--- src/setup_frames.py
+++ src/setup_frames.py
@@ -2,15 +2,15 @@ from ..external import pyqtgraph as pg
 
 from qgis.PyQt.QtWidgets import QVBoxLayout
 
-from .pg_toolbar import CustomToolbar
+from .ui.toolbars import TimeSeriesToolbar
 
 
 def setupTsFrame(ui):
     ui.plot_widget = pg.GraphicsLayoutWidget(parent=ui.frame_plot_ts)
     ui.plot_widget.setBackground('w')
     ui.plot_widget.plot_items = []
-    ui.toolbar = CustomToolbar(ui.plot_widget, ui.frame_plot_ts)
+    ui.time_series_toolbar = TimeSeriesToolbar(ui.frame_plot_ts)
 
     ui.frame_plot_layout = QVBoxLayout(ui.frame_plot_ts)
     ui.frame_plot_layout.addWidget(ui.plot_widget)
-    ui.frame_plot_layout.addWidget(ui.toolbar)
+    ui.frame_plot_layout.addWidget(ui.time_series_toolbar)
