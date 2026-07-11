e394825f71efba37ecd66b5f07fcec3ca3481453
mahmud1 <mahmud1@users.noreply.github.com>
2026-07-11 21:17:19 +0200

    move fit model to toolbar: mirror selected model icon on fit toggle

--- src/ui/toolbars/time_series_toolbar.py
+++ src/ui/toolbars/time_series_toolbar.py
@@ -62,6 +62,7 @@ class TimeSeriesToolbar(QToolBar):
         self.fit_model_button.setMenu(self.fit_model_menu)
         self.fit_model_button.setDefaultAction(self.fit_model_actions["poly-1"])
         self.addWidget(self.fit_model_button)
+        self._updateFitTogglePresentation()
 
         self.seasonal_action = self._createToggleAction(
             ":/icons/icons/fit__add_seasonal.svg",
@@ -135,6 +136,20 @@ class TimeSeriesToolbar(QToolBar):
         action.setChecked(True)
         self.fit_model_button.setDefaultAction(action)
         self.fit_model_group.blockSignals(previous)
+        self._updateFitTogglePresentation()
+
+
+    def _updateFitTogglePresentation(self):
+        """Mirror the selected model action on the fit toggle presentation."""
+        selected_action = next(
+            action for action in self.fit_model_actions.values() if action.isChecked()
+        )
+        model_name = selected_action.text()
+        self.fit_enabled_action.setIcon(selected_action.icon())
+        self.fit_enabled_action.setToolTip(f"Toggle {model_name.lower()} fit")
+        self.fit_enabled_action.setStatusTip(f"Fit using {model_name.lower()} model")
+        self.fit_enabled_action.setWhatsThis(f"Fit using {model_name.lower()} model")
+        self.fit_model_button.setAccessibleName(f"Selected fit model: {model_name}")
 
     def setSeasonalEnabled(self, enabled):
         """Update the seasonal toggle without emitting a user-change signal."""
